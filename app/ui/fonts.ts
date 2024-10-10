cf4401f49f8af19c6527320c3c71a5b6df6ec47b
takumi2003 <takumi20030819@gmail.com>
2024-10-11 01:30:55 +0900

    hoge

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,11 +1,8 @@
-import { Inter } from 'next/font/google';
-import { Lusitana } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-const lusitana = Lusitana({
-    weight: ['400', '700'],
-    subsets: ['latin'],
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
 });
-
-// ここでエクスポートする
-export { inter, lusitana };  // ここでエクスポートされていることを確認
